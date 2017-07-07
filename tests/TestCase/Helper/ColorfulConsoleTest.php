8834865a17cf805913b4414b410c337d1c15f034
Pete Chan <chanpete96@gmail.com>
2017-07-07 16:03:43 +0200

    second try on travis with simple tests

---
+++ tests/TestCase/Helper/ColorfulConsoleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RabbitMQ\Test\TestCase\Helper;
+
+use RabbitMQ\Helper\ColorfulConsole;
+use Cake\TestSuite\TestCase;
+
+class ColorfulConsoleTest extends TestCase
+{
+    public function testVaildState()
+    {
+        $c = new ColorfulConsole();
+        try {
+            ob_start();
+            $c('default', 'white');
+            $c('info', 'cyan');
+            $c('success', 'green');
+            $c('warning', 'yellow');
+            $c('error', 'red');
+            ob_end_clean();
+        }
+        catch(Exception $e) {
+            $this->fail();
+        }
+
+        $this->assertTrue(true);
+    }
+
+    public function testInvaildState()
+    {
+        $c = new ColorfulConsole();
+        $this->expectException(\InvalidArgumentException::class);
+        $c('ex', 'exception');
+    }
+}
