d902e5f9e9677cdc3c4e7c90db48aff4a5baffeb
Pete Chan <chanpete96@gmail.com>
2017-07-10 10:44:46 +0200

    Test Config

---
+++ tests/TestCase/Configure/ConfigTest.php
@@ -0,0 +1,103 @@
+<?php
+namespace RabbitMQ\Test\TestCase\Configure;
+
+use RabbitMQ\Configure\Config;
+use Cake\Core\Configure;
+use Cake\TestSuite\TestCase;
+
+class ConfigTest extends TestCase
+{
+    public function testCakeCanReadConfig()
+    {
+        $this->assertInternalType('array', Configure::read('Riesenia.CakephpRabbitMQ'));
+    }
+
+    public function testGetServerConfig()
+    {
+        $server = Config::getServer();
+
+        $this->assertTextEquals(5672, $server['port']);
+        $this->assertTextEquals('guest', $server['user']);
+        $this->assertTextEquals('guest', $server['user']);
+        $this->assertTextEquals('/', $server['vhost']);
+    }
+
+    public function testDefaultNameGeneration()
+    {
+        $config = Config::get('min');
+
+        $this->assertTextEquals('min_exchange', $config['exchange']['name']);
+        $this->assertTextEquals('min_queue', $config['queue']['name']);
+        $this->assertTextEquals('min_routing_key', $config['routing_key']);
+
+        $this->assertTextEquals('min_retry_exchange', $config['retry_exchange']['name']);
+        $this->assertTextEquals('min_retry_queue', $config['retry_queue']['name']);
+        $this->assertTextEquals('min_retry_routing_key', $config['retry_routing_key']);
+    }
+
+    public function testNoRetryBindingRouteGeneration()
+    {
+        $config = Config::get('min');
+
+        $queueArgs = $config['queue']['arguments'];
+        $this->assertTextEquals(null, $queueArgs['x-dead-letter-exchange']);
+        $this->assertTextEquals(null, $queueArgs['x-dead-letter-routing-key']);
+
+        $retryQueueArgs = $config['retry_queue']['arguments'];
+        $this->assertTextEquals(null, $retryQueueArgs['x-message-ttl']);
+        $this->assertTextEquals(null, $retryQueueArgs['x-dead-letter-exchange']);
+        $this->assertTextEquals(null, $retryQueueArgs['x-dead-letter-routing-key']);
+    }
+
+    public function testRetryBindingRouteGeneration()
+    {
+        $config = Config::get('retry_15s_with_max_3_times');
+        
+        $queueArgs = $config['queue']['arguments'];
+        $this->assertTextEquals([ 'S', 'retry_15s_with_max_3_times_retry_exchange' ], $queueArgs['x-dead-letter-exchange']);
+        $this->assertTextEquals([ 'S', 'retry_15s_with_max_3_times_retry_routing_key' ], $queueArgs['x-dead-letter-routing-key']);
+
+        $retryQueueArgs = $config['retry_queue']['arguments'];
+        $this->assertTextEquals([ 'I', 15 * 1000 ], $retryQueueArgs['x-message-ttl']);
+        $this->assertTextEquals([ 'S', 'retry_15s_with_max_3_times_exchange' ], $retryQueueArgs['x-dead-letter-exchange']);
+        $this->assertTextEquals([ 'S', 'retry_15s_with_max_3_times_routing_key' ], $retryQueueArgs['x-dead-letter-routing-key']);
+    }
+
+    public function testOverrideDefaultConfig()
+    {
+        $config = Config::get('custom_setting');
+
+        $this->assertTextEquals('ex', $config['exchange']['name']);
+        $this->assertTextEquals('q', $config['queue']['name']);
+        $this->assertTextEquals('rk', $config['routing_key']);
+
+        $this->assertTextEquals('re_ex', $config['retry_exchange']['name']);
+        $this->assertTextEquals('re_q', $config['retry_queue']['name']);
+        $this->assertTextEquals('re_rk', $config['retry_routing_key']);
+    }
+
+    public function testOverridedRetryBindingRouteGeneration()
+    {
+        $config = Config::get('custom_setting');
+
+        $queueArgs = $config['queue']['arguments'];
+        $this->assertTextEquals([ 'S', 're_ex' ], $queueArgs['x-dead-letter-exchange']);
+        $this->assertTextEquals([ 'S', 're_rk' ], $queueArgs['x-dead-letter-routing-key']);
+
+        $retryQueueArgs = $config['retry_queue']['arguments'];
+        $this->assertTextEquals([ 'S', 'ex' ], $retryQueueArgs['x-dead-letter-exchange']);
+        $this->assertTextEquals([ 'S', 'rk' ], $retryQueueArgs['x-dead-letter-routing-key']);
+    }
+
+    public function testGetNotExistKey()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        Config::get('NotExist');
+    }
+
+    public function testGetServerConfigWithGetFunction()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        Config::get('server');
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,1 +1,16 @@
 <?php
+
+define('ROOT', dirname(__DIR__));
+define('CONFIG', __DIR__ . DS . 'config' . DS);
+
+require ROOT . DS . 'vendor' . DS . 'autoload.php';
+
+use Cake\Core\Configure;
+use Cake\Core\Configure\Engine\PhpConfig;
+
+try {
+    Configure::config('default', new PhpConfig());
+    Configure::load('test', 'default', false);
+} catch (\Exception $e) {
+    die($e->getMessage() . "\n");
+}
