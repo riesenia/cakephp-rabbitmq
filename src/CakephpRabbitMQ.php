1a6da37b47292c64e468b26048799762295d48af
Tomas Saghy <tomas.saghy@gmail.com>
2017-07-11 11:04:41 +0200

    comments

--- src/CakephpRabbitMQ.php
+++ src/CakephpRabbitMQ.php
@@ -12,8 +12,7 @@ use RabbitMQ\Helper\MeaningfulTime;
 class CakephpRabbitMQ
 {
     /**
-     * Read the configs according to the key provided and listen to them,
-     * listen all if not key is specified
+     * Read the configs and start listening to defined queues
      *
      * @param  array $keys
      * @return void
@@ -23,7 +22,7 @@ class CakephpRabbitMQ
         $server = Config::getServer();
         $configs = empty($keys) ? Config::getAllConfigs() : Config::getConfigs($keys);
 
-        // Generate callback according to provided configs
+        // generate callbacks
         foreach ($configs as $key => $config) {
             $configs[$key]['_callback'] = static::_generateInternalCallback($key, $config);
         }
@@ -36,7 +35,7 @@ class CakephpRabbitMQ
      *
      * @param  string $key
      * @param  array  $config
-     * @return function($message)
+     * @return callable
      */
     protected static function _generateInternalCallback(string $key, array $config)
     {
@@ -46,7 +45,7 @@ class CakephpRabbitMQ
         $m = new MeaningfulTime();
         $retryTime = $m($config['retry_time'], 'ms');
 
-        // Internal callback function
+        // callback function
         return function ($message) use ($key, $callback, $retryMax, $retryTime) {
             $c = new ColorfulConsole();
             $c('default', sprintf("[*] Queue '%s' received message : '%s'", $key, $message->body));
@@ -57,30 +56,28 @@ class CakephpRabbitMQ
                 $xDeath = $headers->getNativeData()['x-death'];
                 $retryCount = $xDeath[1]['count'];
             } catch (\OutOfBoundsException $e) {
-                // The message would not have the header at first time
+                // the message would not have the header at first time
                 $retryCount = 0;
             }
 
-            // On failed
+            // failed
             if ($result != 0) {
-                // Retry
+                // retry
                 if ($retryCount < $retryMax) {
                     $c('warning', sprintf("[!] Failed to process the message, retry after %s (retried %d times)", $retryTime, $retryCount));
-                    // Exceeded maximum retry
                 } else {
                     $c('error', sprintf("[X] Failed to process the message and exceeded maximum retry count of %d, dropping the message", $retryMax));
-                    // Drop the message by sending ack
+                    // drop the message by sending ack
                     $result = 0;
                 }
-                // On success
             } else {
-                $c('success', sprintf("[√] Success to process the message"));
+                $c('success', sprintf("[√] Successfully processed the message"));
             }
 
             echo "\n";
 
             if ($result != 0) {
-                // Redirect to the retry queue if non-zero value returned
+                // redirect to the retry queue if non-zero value returned
                 $message->delivery_info['channel']->basic_reject($message->delivery_info['delivery_tag'], false);
             } else {
                 $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
@@ -89,27 +86,25 @@ class CakephpRabbitMQ
     }
 
     /**
-     * Generate callback according to the config of user
+     * Generate callback according according to the callback type provided
      *
      * @param string $key
      * @param array $config
-     * @return callable|function($message)
+     * @return callable
      */
     protected static function _generateUserCallback(string $key, array $config)
     {
-        // Generate the callback according to the callback type provided
-
+        // callable
         if (isset($config['callback'])) {
-            // Callable
             $callback = $config['callback'];
             if (!is_callable($callback)) {
-                throw new \InvalidArgumentException('The callback provided in queue "' . $key . '" is not a valid callable');
+                throw new \InvalidArgumentException('The callback provided in queue "' . $key . '" is not a valid callable!');
             }
             return $callback;
         }
-        
+
+        // command
         if (isset($config['command'])) {
-            // Command
             $command = $config['command'];
             $callback = function ($message) use ($command) {
                 exec($command . ' ' . $message->body, $output, $result);
@@ -117,9 +112,9 @@ class CakephpRabbitMQ
             };
             return $callback;
         }
-        
+
+        // cakephp command
         if (isset($config['cake_command'])) {
-            // Cakephp command
             $cakeCommand = $config['cake_command'];
             $callback = function ($message) use ($cakeCommand) {
                 exec('bin' . DS . 'cake ' . $cakeCommand . ' ' . $message->body, $output, $result);
